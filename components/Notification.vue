4e41c890ab5b5d71b70c849243c26176a156ef6e
Dendi Handian <handian.dendi@gmail.com>
2018-08-19 11:36:13 +0700

    user registration

---
+++ components/Notification.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="notification is-danger">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Notification',
+    props: ['message']
+  }
+</script>
+
+<style lang="css">
+</style>
